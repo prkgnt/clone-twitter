7c493dae0c326fbb1d6b4c8d0b0e999619b8d726
prkgnt <pgt258258@gmail.com>
2023-03-16 19:36:43 +0900

    email 로그인 기능 간단 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name == "email") {
+      setEmail(value);
+    }
+    if (name == "password") {
+      setPassword(value);
+    }
+    //event.target => 이벤트가 일어나는 타겟(객체) 의미
+    console.log(event.target.name);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="E-mail"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" placeholder="Log-In" />
+      </form>
+      <div>
+        <button>LogIn with Google</button>
+        <button>LogIn with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
