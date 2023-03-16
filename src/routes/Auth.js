ddd05ae0ff11d13439ccf27e1d2de66f3e1c556e
prkgnt <pgt258258@gmail.com>
2023-03-16 19:59:37 +0900

    이메일 회원가입 기능 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { authService } from "../firebase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -16,15 +19,29 @@ const Auth = () => {
     //event.target => 이벤트가 일어나는 타겟(객체) 의미
     console.log(event.target.name);
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
           name="email"
-          type="text"
+          type="email"
           placeholder="E-mail"
           required
           value={email}
@@ -38,7 +55,10 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" placeholder="Log-In" />
+        <input
+          type="submit"
+          value={newAccount ? "Create new account" : "Log-In"}
+        />
       </form>
       <div>
         <button>LogIn with Google</button>
