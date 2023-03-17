23a4dae429b64682f0d3f1cc9bcad1956e593a68
prkgnt <pgt258258@gmail.com>
2023-03-17 21:32:47 +0900

    firestore에서 데이터 받아와서 뿌리기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../firebase";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    //collection.get() 은 전체 데이터를 한번에 가져오는게 아니라 하나씩 가져옴
+    //그래서 forEach 써야됨
+    const dbTweets = await dbService.collection("tweet").get();
+    dbTweets.forEach((doc) => {
+      const tweetsObject = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setTweets((prev) => [tweetsObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    setTweets([]);
+    getTweets();
+  }, []);
+
   const onChange = (event) => {
     const {
       target: { value },
@@ -29,7 +49,15 @@ const Home = () => {
         />
         <input type="submit" value="tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default Home;
