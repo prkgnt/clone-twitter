a80f8985a1668dcdcd47a342cb1400bbec6616e8
prkgnt <pgt258258@gmail.com>
2023-03-17 19:52:03 +0900

    Profile 페이지 및 로그아웃 기능 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,26 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
-
+import Profile from "../routes/Profile";
 import App from "./App";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
-            <Route exact path={"/"}>
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
-          <Route exact path={"/"}>
+          <Route exact path="/">
             <Auth></Auth>
           </Route>
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,19 @@
 import React from "react";
+import { authService } from "../firebase";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    //useHistory 훅을 이용하면 원하는 페이지로 이동 가능
+    //또는 Redirect 사용하면 됨
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 export default Profile;
