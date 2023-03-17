3c2e576a80bea65990df5b4ed8474ae25a0da2aa
prkgnt <pgt258258@gmail.com>
2023-03-17 20:55:57 +0900

    firestore 이용해 트윗 데이터베이스에 올리기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../firebase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweet").add({
+      tweet,
+      createAt: Date.now(),
+    });
+    setTweet("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
