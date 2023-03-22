e1144fc6938e67ac8d47b3dc38fbb28bdd211dda
prkgnt <pgt258258@gmail.com>
2023-03-22 19:53:11 +0900

    안쓰는 스테이트 만들어서 렌더링 하기

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,19 @@ function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [userObj, setUserObj] = useState(null);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     //userState가 바뀌었을 경우
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
-        setUserObj({
-          displayName: user.displayName,
-          uid: user.uid,
-          updateProfile: (args) => user.updateProfile(args),
-        });
+        // setUserObj({
+        //   displayName: user.displayName,
+        //   uid: user.uid,
+        //   updateProfile: (args) => user.updateProfile(args),
+        // });
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -27,11 +29,14 @@ function App() {
 
   const refreshUser = () => {
     const user = authService.currentUser;
-    setUserObj({
-      displayName: user.displayName,
-      uid: user.uid,
-      updateProfile: (args) => user.updateProfile(args),
-    });
+    // setUserObj({
+    //   displayName: user.displayName,
+    //   uid: user.uid,
+    //   updateProfile: (args) => user.updateProfile(args),
+    // });
+    setUserObj(user);
+    //안쓰는 스테이트 같이 업데이트 해서 렌더링 강제하기
+    setName(user.displayName);
   };
 
   //return {} => 이건 안됨
